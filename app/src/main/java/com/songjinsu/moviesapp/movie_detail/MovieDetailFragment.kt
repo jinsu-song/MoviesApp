ad81370d73d439f3b13080d38334692e83abe19e
songjinsu486 <songjinsu486@juglans.co.kr>
2024-05-22 18:26:10 +0900

    영화 상세 화면 구현

--- app/src/main/java/com/songjinsu/moviesapp/movie_detail/MovieDetailFragment.kt
+++ app/src/main/java/com/songjinsu/moviesapp/movie_detail/MovieDetailFragment.kt
@@ -7,9 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.songjinsu.moviesapp.MainViewModel
 import com.songjinsu.moviesapp.databinding.MovieDetailFragmentBinding
+import com.songjinsu.moviesapp.datamodel.MovieDetailResponse
 
 class MovieDetailFragment(val vm: MainViewModel, val movieId: String) : Fragment() {
     private lateinit var binding: MovieDetailFragmentBinding
+    private val movieDetailViewModel = MovieDetailViewModel()
+    private lateinit var movieDetail : MovieDetailResponse
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,8 +28,27 @@ class MovieDetailFragment(val vm: MainViewModel, val movieId: String) : Fragment
 
     fun init() {
 
+        movieDetailViewModel.getMovieDetail(movieId, requireContext())
+
         binding.btnGoBack.setOnClickListener {
             vm.popFragment()
         }
+
+        movieDetailViewModel.movieDetailLiveData.observe(viewLifecycleOwner) {
+            movieDetail = it
+
+            binding.tvTitle.text = movieDetail.title
+            binding.tvVoteAverage.text = movieDetail.voteAverage.toString()
+            binding.tvOverview.text = movieDetail.overview
+
+            // 이미지 불러오기
+            movieDetail.posterPath?.let { it1 -> movieDetailViewModel.loadPoster(it1, requireContext()) }
+        }
+
+        movieDetailViewModel.posterLiveData.observe(viewLifecycleOwner) {
+            binding.ivPoster.setImageBitmap(it)
+        }
     }
+
+
 }
--- app/src/main/java/com/songjinsu/moviesapp/movie_detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/songjinsu/moviesapp/movie_detail/MovieDetailViewModel.kt
@@ -1,9 +1,12 @@
 package com.songjinsu.moviesapp.movie_detail
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.songjinsu.moviesapp.common.App
 import com.songjinsu.moviesapp.datamodel.MovieDetailResponse
 import com.songjinsu.moviesapp.net.HttpRequest
 import com.songjinsu.moviesapp.net.Paths
@@ -12,6 +15,7 @@ class MovieDetailViewModel : ViewModel() {
 
     private val call = HttpRequest
     val movieDetailLiveData = MutableLiveData<MovieDetailResponse>()
+    val posterLiveData = MutableLiveData<Bitmap>()
 
     // 영화 정보 불러오기
     fun getMovieDetail(movieId: String, context: Context) {
@@ -32,5 +36,27 @@ class MovieDetailViewModel : ViewModel() {
                 Toast.makeText(context, "$methodName >>>> ${error.message}", Toast.LENGTH_SHORT).show()
             }
         )
+    }
+
+    fun loadPoster(imageName: String, context: Context) {
+        val baseUrl = App.configuration.images?.baseUrl
+        val url = "${baseUrl}w500${imageName}"
+        if (baseUrl != null) {
+
+            call.imageLoad(
+                url,
+                Bitmap::class.java,
+                context,
+                { bitmap ->
+                    if (bitmap != null) {
+                        posterLiveData.postValue(bitmap)
+                    }
+
+                }, { error ->
+                    Log.d("Errorrrrr", "Error Message >>>>> : ${error.message}")
+
+                }
+            )
+        }
     }
 }
